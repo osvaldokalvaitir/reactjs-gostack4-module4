1ca06b4a8ab730dbd50fcedfc7a163deb9b63cb8
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-01-19 12:35:10 -0200

    Estilizando página Playlist

--- src/pages/browse/index.js
+++ src/pages/browse/index.js
@@ -9,7 +9,7 @@ const Browse = () => (
     <Title>Navegar</Title>
 
     <List>
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img
           src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
           alt="Playlist"
@@ -17,7 +17,7 @@ const Browse = () => (
         <strong>Rock dos bons</strong>
         <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
       </Playlist>
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img
           src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
           alt="Playlist"
@@ -25,7 +25,7 @@ const Browse = () => (
         <strong>Rock dos bons</strong>
         <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
       </Playlist>
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img
           src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
           alt="Playlist"
@@ -33,7 +33,7 @@ const Browse = () => (
         <strong>Rock dos bons</strong>
         <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial!</p>
       </Playlist>
-      <Playlist href="">
+      <Playlist to="/playlists/1">
         <img
           src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
           alt="Playlist"
---
+++ src/pages/playlist/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import { Container, Header, SongList } from './styles';
+
+import ClockIcon from '../../assets/images/clock.svg';
+import PlusIcon from '../../assets/images/plus.svg';
+
+const Playlist = () => (
+  <Container>
+    <Header>
+      <img
+        src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
+        alt="Playlist"
+      />
+
+      <div>
+        <span>PLAYLIST</span>
+        <h1>Rock Forever</h1>
+        <p>13 músicas</p>
+
+        <button>PLAY</button>
+      </div>
+    </Header>
+
+    <SongList cellPadding={0} cellSpacing={0}>
+      <thead>
+        <th />
+        <th>Título</th>
+        <th>Artista</th>
+        <th>Álbum</th>
+        <th>
+          <img src={ClockIcon} alt="Duração" />
+        </th>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+        <tr>
+          <td>
+            <img src={PlusIcon} alt="Adicionar" />
+          </td>
+          <td>Papercut</td>
+          <td>Linkin Park</td>
+          <td>Hybrid Theory</td>
+          <td>3:26</td>
+        </tr>
+      </tbody>
+    </SongList>
+  </Container>
+);
+
+export default Playlist;
