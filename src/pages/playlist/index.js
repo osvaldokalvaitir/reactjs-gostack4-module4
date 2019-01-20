eb5b1b365adcafa53c3055af623e242d31d621cd
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-01-20 14:06:17 -0200

    Música selecionada/tocando

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -6,7 +6,7 @@ import { bindActionCreators } from 'redux';
 import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
 import { Creators as PlayerActions } from '../../store/ducks/player';
 
-import { Container, Header, SongList } from './styles';
+import { Container, Header, SongList, SongItem } from './styles';
 
 import Loading from '../../components/Loading';
 
@@ -38,7 +38,14 @@ class Playlist extends Component {
       loading: PropTypes.bool,
     }).isRequired,
     loadSong: PropTypes.func.isRequired,
+    currentSong: PropTypes.shape({
+      id: PropTypes.number,
+    }).isRequired,
   };
+
+  state = {
+    selectedSong: null,
+  }
 
   componentDidMount() {
     this.loadPlaylistDetails();
@@ -91,7 +98,13 @@ class Playlist extends Component {
               </tr>
             ) : (
               playlist.songs.map(song => (
-                <tr key={song.id} onDoubleClick={() => this.props.loadSong(song)}>
+                <SongItem 
+                  key={song.id}
+                  onClick={() => this.setState({ selectedSong: song.id })}
+                  onDoubleClick={() => this.props.loadSong(song)}
+                  selected={this.state.selectedSong === song.id}
+                  playing={this.props.currentSong && this.props.currentSong.id === song.id}
+                >
                   <td>
                     <img src={PlusIcon} alt="Adicionar" />
                   </td>
@@ -99,7 +112,7 @@ class Playlist extends Component {
                   <td>{song.author}</td>
                   <td>{song.album}</td>
                   <td>3:26</td>
-                </tr>
+                </SongItem>
               ))
             )}
           </tbody>
@@ -121,6 +134,7 @@ class Playlist extends Component {
 
 const mapStateToProps = state => ({
   playlistDetails: state.playlistDetails,
+  currentSong: state.player.currentSong,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({ ...PlaylistDetailsActions, ...PlayerActions }, dispatch);
--- src/pages/playlist/styles.js
+++ src/pages/playlist/styles.js
@@ -81,12 +81,16 @@ export const SongList = styled.table`
       text-align: right;
     }
   }
+`;
 
-  tbody td {
+export const SongItem = styled.tr`
+  td {
     border-top: 1px solid #282828;
     font-size: 13px;
     padding: 0 10px;
     line-height: 40px;
+    background: ${props => (props.selected ? '#282828' : 'transparent')}
+    color: ${props => (props.playing ? '#1ED760' : '#FFF')};
 
     &:first-child {
       width: 80px;
@@ -98,7 +102,7 @@ export const SongList = styled.table`
     }
   }
 
-  tbody tr:hover td {
+  &:hover td {
     background: #282828;
   }
 `;
