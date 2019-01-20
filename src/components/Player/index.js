82a9c83dd2f78c36c75ba3b7a5177641234e373a
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-01-20 17:17:50 -0200

    Slider de duração

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -19,7 +19,20 @@ import PauseIcon from '../../assets/images/pause.svg';
 import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
-const Player = ({ player, play, pause, next, prev, playing, position, duration }) => (
+const Player = ({ 
+  player,
+  play,
+  pause,
+  next,
+  prev,
+  playing,
+  position,
+  duration,
+  handlePosition,
+  setPosition,
+  positionShown,
+  progress,
+}) => (
   <Container>
     {!!player.currentSong && (
       <Sound
@@ -27,6 +40,7 @@ const Player = ({ player, play, pause, next, prev, playing, position, duration }
         playStatus={player.status}
         onFinishedPlaying={next}
         onPlaying={playing}
+        position={player.position}
       />
     )}
 
@@ -69,12 +83,16 @@ const Player = ({ player, play, pause, next, prev, playing, position, duration }
       </Controls>
 
       <Time>
-        <span>{position}</span>
+        <span>{positionShown || position}</span>
         <ProgressSlider>
           <Slider
             railStyle={{ background: '#404040', borderRadius: 10 }}
             trackStyle={{ background: '#1ED760' }}
             handleStyle={{ border: 0 }}
+            max={1000}
+            onChange={value => handlePosition(value / 1000)}
+            onAfterChange={value => setPosition(value / 1000)}
+            value={progress}
           />
         </ProgressSlider>
         <span>{duration}</span>
@@ -110,9 +128,15 @@ Player.propTypes = {
   playing: PropTypes.func.isRequired,
   position: PropTypes.string.isRequired,
   duration: PropTypes.string.isRequired,
+  handlePosition: PropTypes.func.isRequired,
+  setPosition: PropTypes.func.isRequired,
+  positionShown: PropTypes.string.isRequired,
+  progress: PropTypes.number.isRequired,
 };
 
 function msToTime(duration) {
+  if (!duration) return null;
+
   let seconds = parseInt((duration / 1000) % 60, 10);
   const minutes = parseInt(((duration / (1000 * 60)) % 60), 10);
 
@@ -125,6 +149,8 @@ const mapStateToProps = state => ({
   player: state.player,
   position: msToTime(state.player.position),
   duration: msToTime(state.player.duration),
+  positionShown: msToTime(state.player.positionShown),
+  progress: parseInt((state.player.positionShown || state.player.position) * (1000 / state.player.duration), 10) || 0,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -7,6 +7,8 @@ export const Types = {
   NEXT: 'player/NEXT',
   PREV: 'player/PREV',
   PLAYING: 'player/PLAYING',
+  HANDLE_POSITION: 'player/HANDLE_POSITION',
+  SET_POSITION: 'player/SET_POSITION',
 };
 
 const INITIAL_STATE = {
@@ -14,6 +16,7 @@ const INITIAL_STATE = {
   list: [],
   status: Sound.status.PLAYING,
   position: null,
+  positionShown: null,
   duration: null,
 };
 
@@ -35,7 +38,12 @@ export default function player(state = INITIAL_STATE, action) {
       const prev = state.list[currentIndex - 1];
 
       if (prev) {
-        return { ...state, currentSong: prev, status: Sound.status.PLAYING };
+        return {
+          ...state,
+          currentSong: prev,
+          status: Sound.status.PLAYING,
+          position: 0,
+        };
       }
 
       return state;
@@ -45,13 +53,22 @@ export default function player(state = INITIAL_STATE, action) {
       const next = state.list[currentIndex + 1];
 
       if (next) {
-        return { ...state, currentSong: next, status: Sound.status.PLAYING };
+        return {
+          ...state,
+          currentSong: next,
+          status: Sound.status.PLAYING,
+          position: 0,
+        };
       }
 
       return state;
     }
     case Types.PLAYING:
       return { ...state, ...action.payload };
+    case Types.HANDLE_POSITION:
+      return { ...state, positionShown: state.duration * action.payload.percent };
+    case Types.SET_POSITION:
+      return { ...state, position: state.duration * action.payload.percent, positionShown: null };
     default:
       return state;
   }
@@ -74,5 +91,15 @@ export const Creators = {
   playing: ({ position, duration }) => ({
     type: Types.PLAYING,
     payload: { position, duration },
+  }),
+
+  handlePosition: percent => ({
+    type: Types.HANDLE_POSITION,
+    payload: { percent },
+  }),
+
+  setPosition: percent => ({
+    type: Types.SET_POSITION,
+    payload: { percent },
   }),
 };
